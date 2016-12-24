0e05347ca8f0114fd1e25426b06132c7e3d4f7b1
Nexrem <kristiana2000@abv.bg>
2016-12-24 19:17:47 +0200

    parse plist

--- index.js
+++ index.js
@@ -1,43 +1,67 @@
 var fs = require("fs");
 PNG = require('pngjs').PNG;
+var plist = require('plist');
 
-var datasheet = fs.readFileSync("./input/sheet.json", "utf8").replace(/"rotated"/g,'"textureRotated"').replace(/"frame"/g, '"textureRect"'); // read input datasheet
-var frames = JSON.parse(datasheet)["frames"]; // parse the frames (coords)
-var Resources = Object.keys(frames); // get all object names
-var sheet = PNG.sync.read( fs.readFileSync('./input/sheet.png') ); // loads in the datasheet
+var frames;
+var Resources;
+var sheet;
 
-// iterate through all objects
-for (i=0; i<Resources.length; i++) {
-  var objectName = Resources[i]; // get current object key
-  var objectData = frames[objectName]; // get current object data
+var inputFiles = fs.readdirSync("./input"); // get files in input
 
-  // don't extract glows
-  if ( !objectName.includes("glow") ) {
+// remove persist
+if (inputFiles.indexOf(".persist") > -1) {
+    inputFiles.splice(inputFiles.indexOf(".persist"), 1);
+}
+if ( inputFiles[0].includes(".plist") && inputFiles.indexOf(inputFiles[0].replace(".plist", ".png")) > -1 ||
+     inputFiles[0].includes(".png") && inputFiles.indexOf(inputFiles[0].replace(".png", ".plist")) > -1 &&
+      inputFiles.length >= 2) {
+       start(inputFiles[0].replace(".plist", "").replace(".png",""));
+} else {
+  console.log("ERROR! Are your files named the same? Not enoug files? Minimum 2. Plist and PNG");
+}
 
-    // get X, Y, width and height values
-    var objX = parseCoords(objectData.textureRect)[0][0]
-    var objY = parseCoords(objectData.textureRect)[0][1]
-    var objW = parseCoords(objectData.textureRect)[1][0]
-    var objH = parseCoords(objectData.textureRect)[1][1]
+function start(file) {
+  var datasheet = fs.readFileSync("./input/" + file + ".plist", "utf8").replace(/"rotated"/g,'"textureRotated"').replace(/"frame"/g, '"textureRect"'); // read input datasheet
+  frames = plist.parse(datasheet)["frames"]; // parse the frames (coords)
+  Resources = Object.keys(frames); // get all object names
+  sheet = PNG.sync.read( fs.readFileSync('./input/' + file + ".png") ); // loads in the datasheet
+  extract();
+};
 
-    if (objectData.textureRotated) {
-      var dst = new PNG({width: objH, height: objW, filterType:4} ); //creates a blank PNG object
-      PNG.bitblt(sheet, dst, objX, objY, objH, objW, 0, 0); // pumps into new image with flipped width and height
-      dst = rotateImage(dst); // pass to rotate
-    } else {
-      var dst = new PNG({width: objW, height: objH, filterType:4} ); //creates a blank PNG object
-      PNG.bitblt(sheet, dst, objX, objY, objW, objH, 0, 0); // pump into new image
+function extract() {
+  // iterate through all objects
+  for (i=0; i<Resources.length; i++) {
+    var objectName = Resources[i]; // get current object key
+    var objectData = frames[objectName]; // get current object data
+
+    // don't extract glows
+    if ( !objectName.includes("glow") ) {
+
+      // get X, Y, width and height values
+      var objX = parseCoords(objectData.textureRect)[0][0]
+      var objY = parseCoords(objectData.textureRect)[0][1]
+      var objW = parseCoords(objectData.textureRect)[1][0]
+      var objH = parseCoords(objectData.textureRect)[1][1]
+
+      if (objectData.textureRotated) {
+        var dst = new PNG({width: objH, height: objW, filterType:4} ); //creates a blank PNG object
+        PNG.bitblt(sheet, dst, objX, objY, objH, objW, 0, 0); // pumps into new image with flipped width and height
+        dst = rotateImage(dst); // pass to rotate
+      } else {
+        var dst = new PNG({width: objW, height: objH, filterType:4} ); //creates a blank PNG object
+        PNG.bitblt(sheet, dst, objX, objY, objW, objH, 0, 0); // pump into new image
+      };
+
+      // check if directory for file exists if not create
+      var dir = "./out/" + objectName.split("_")[0] + "/"
+      if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+      }
+
+      fs.writeFileSync(dir + objectName, PNG.sync.write(dst)); // write extracted file
+
+      console.log("Finished " + objectName)
     };
-
-    // check if directory for file exists if not create
-    var dir = "./out/" + objectName.split("_")[0] + "/"
-    if (!fs.existsSync(dir)){
-      fs.mkdirSync(dir);
-    }
-
-    fs.writeFileSync(dir + objectName, PNG.sync.write(dst)); // write extracted file
-
-    console.log("Finished " + objectName)
   };
 };
 
